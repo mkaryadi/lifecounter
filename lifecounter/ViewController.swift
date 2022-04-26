505a67052a544f8d643c4e81b3c51d162df3f552
omniFatal <matthew.t.karyadi@gmail.com>
2022-04-26 16:39:37 -0700

    Add functionality to all custom buttons and reset life total with alert

--- lifecounter/ViewController.swift
+++ lifecounter/ViewController.swift
@@ -41,22 +41,35 @@ class ViewController: UIViewController {
         if toUpdate == 1 {
             playerOneLife.text = String(playerOneLifeTotal)
             if playerOneLifeTotal <= 0 {
-                losingLabel.text = "Player 1 Loses!"
+                let alert = UIAlertController(title: "Game Over!", message: "Player One Loses.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: {_ in
+                    self.reset()
+                }))
+                self.present(alert, animated: true)
             }
         } else if toUpdate == 2 {
             playerTwoLife.text = String(playerTwoLifeTotal)
             if playerTwoLifeTotal <= 0 {
-                losingLabel.text = "Player 2 Loses!"
+                let alert = UIAlertController(title: "Game Over!", message: "Player Two Loses.", preferredStyle: .alert)
+                alert.addAction(UIAlertAction(title: "Dismiss", style: .default, handler: { _ in
+                    self.reset()
+                }))
+                self.present(alert, animated: true)
             }
         } else {
             print("Tried to update an invalid life total, ignoring...")
         }
     }
     
-    @IBAction func playerOneMinusCustom(_ sender: Any) {
-        playerOneLifeTotal -= 5
+    
+    func reset () {
+        //TODO: Impliment more complicated resetting behavior
+        playerOneLifeTotal = 20
+        playerTwoLifeTotal = 20
         updateLifeTotal(1)
+        updateLifeTotal(2)
     }
+
     
     @IBAction func playerOneMinusOne(_ sender: Any) {
         playerOneLifeTotal -= 1
@@ -68,38 +81,46 @@ class ViewController: UIViewController {
         updateLifeTotal(1)
     }
     
+    @IBAction func playerOneMinusCustom(_ sender: Any) {
+        let alert = UIAlertController(title: "Subtract Life From Player 2", message: "Subtract how much life?", preferredStyle: .alert)
+        var toSub = 0
+        
+        alert.addAction(UIAlertAction(title: "OK",
+                                      style: .default,
+                                      handler: {_ in
+            toSub = Int(alert.textFields![0].text!) ?? 0
+            NSLog("\"OK\" pressed. Subtracting \(toSub) life...")
+            self.playerOneLifeTotal -= toSub
+            self.updateLifeTotal(1)
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Cancel",
+                                      style: .default,
+                                      handler: {_ in
+            NSLog("\"Cancel\" pressed.")
+        }))
+        
+        alert.addTextField { textField in
+            textField.keyboardType = UIKeyboardType.numberPad
+        }
+        
+        self.present(alert, animated: true)
+    }
+    
+    
     @IBAction func playerOnePlusCustom(_ sender: Any) {
-        playerOneLifeTotal += 5
-        updateLifeTotal(1)
-    }
-    
-    
-    @IBAction func playerTwoMinusCustom(_ sender: Any) {
-        playerTwoLifeTotal -= 5
-        updateLifeTotal(2)
-    }
-    
-    @IBAction func playerTwoMinusOne(_ sender: Any) {
-        playerTwoLifeTotal -= 1
-        updateLifeTotal(2)
-    }
-    
-    @IBAction func playerTwoPlusOne(_ sender: Any) {
-        playerTwoLifeTotal += 1
-        updateLifeTotal(2)
-    }
-    
-    @IBAction func playerTwoPlusCustom(_ sender: Any) {
-        let alert = UIAlertController(title: "Add Life To Player 2", message: "Add how much life?", preferredStyle: .alert)
+        let alert = UIAlertController(title: "Add Life To Player 1", message: "Add how much life?", preferredStyle: .alert)
         var toAdd = 0
+    
         alert.addAction(UIAlertAction(title: "OK",
                                       style: .default,
                                       handler: {_ in
             toAdd = Int(alert.textFields![0].text!) ?? 0
             NSLog("\"OK\" pressed. Adding \(toAdd) life...")
-            self.playerTwoLifeTotal += toAdd
-            self.updateLifeTotal(2)
+            self.playerOneLifeTotal += toAdd
+            self.updateLifeTotal(1)
         }))
+    
         alert.addAction(UIAlertAction(title: "Cancel",
                                       style: .default,
                                       handler: {_ in
@@ -109,12 +130,76 @@ class ViewController: UIViewController {
         alert.addTextField { textField in
             textField.keyboardType = UIKeyboardType.numberPad
         }
-        self.present(alert, animated: true, completion: {
-            NSLog("The completion handler fired")
-        })
+        
+        self.present(alert, animated: true)
+        
     }
+    
+    
+    
+    @IBAction func playerTwoMinusOne(_ sender: Any) {
+        playerTwoLifeTotal -= 1
+        updateLifeTotal(2)
+    }
+    
+    
+    @IBAction func playerTwoPlusOne(_ sender: Any) {
+        playerTwoLifeTotal += 1
+        updateLifeTotal(2)
+    }
+    
+    
+    @IBAction func playerTwoMinusCustom(_ sender: Any) {
+        let alert = UIAlertController(title: "Subtract Life From Player 2", message: "Subtract how much life?", preferredStyle: .alert)
+        var toSub = 0
+        
+        alert.addAction(UIAlertAction(title: "OK",
+                                      style: .default,
+                                      handler: {_ in
+            toSub = Int(alert.textFields![0].text!) ?? 0
+            NSLog("\"OK\" pressed. Subtracting \(toSub) life...")
+            self.playerTwoLifeTotal -= toSub
+            self.updateLifeTotal(2)
+        }))
+        
+        alert.addAction(UIAlertAction(title: "Cancel",
+                                      style: .default,
+                                      handler: {_ in
+            NSLog("\"Cancel\" pressed.")
+        }))
+        
+        alert.addTextField { textField in
+            textField.keyboardType = UIKeyboardType.numberPad
+        }
+        
+        self.present(alert, animated: true)
+    }
+    
+    
+    @IBAction func playerTwoPlusCustom(_ sender: Any) {
+        let alert = UIAlertController(title: "Add Life To Player 2", message: "Add how much life?", preferredStyle: .alert)
+        var toAdd = 0
+    
+        alert.addAction(UIAlertAction(title: "OK",
+                                      style: .default,
+                                      handler: {_ in
+            toAdd = Int(alert.textFields![0].text!) ?? 0
+            NSLog("\"OK\" pressed. Adding \(toAdd) life...")
+            self.playerTwoLifeTotal += toAdd
+            self.updateLifeTotal(2)
+        }))
+    
+        alert.addAction(UIAlertAction(title: "Cancel",
+                                      style: .default,
+                                      handler: {_ in
+            NSLog("\"Cancel\" pressed.")
+        }))
 
-    
-    
+        alert.addTextField { textField in
+            textField.keyboardType = UIKeyboardType.numberPad
+        }
+        
+        self.present(alert, animated: true)
+    }
 }
 
